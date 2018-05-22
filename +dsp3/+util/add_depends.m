7319c8c52527c2e2a705339de30ffd73512f35d6
nfagan <nick@fagan.org>
2018-05-22 11:18:35 -0400

    update depends

--- +dsp3/+util/add_depends.m
+++ +dsp3/+util/add_depends.m
@@ -12,4 +12,12 @@ for i = 1:numel(repos)
   addpath( genpath(fullfile(repo_dir, repos{i})) );
 end
 
+if ( isfield(conf.DEPENDS, 'others') )
+  try
+    cellfun( @(x) addpath(genpath(x)), conf.DEPENDS.others );
+  catch err
+    warning( err.message );
+  end
+end
+
 end
