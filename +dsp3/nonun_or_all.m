e5d88da21829f78b5adbe3e1a0b180f224b66d5d
nfagan <nick@fagan.org>
2018-07-27 16:13:18 -0400

    update nonun or all

--- +dsp3/nonun_or_all.m
+++ +dsp3/nonun_or_all.m
@@ -1,10 +1,13 @@
-function c = nonun_or_all(labs, cats)
+function c = nonun_or_all(labs, cats, always)
 
 %   NONUN_OR_ALL -- Filter out uniform categories, if possible.
 %
 %     c = nonun_or_all( labs, cats ) returns the subset of categories
 %     `cats` that are non-uniform in the fcat object `labs`. If none of 
 %     `cats` are non-uniform, then `c` is `cats`.
+%
+%     c = nonun_or_all( ..., always_include ) includes the categories
+%     `always_include` regardless of whether they are uniform.
 %
 %     A non-uniform category is one for which multiple different labels are
 %     present entries in the category.
@@ -14,10 +17,18 @@ function c = nonun_or_all(labs, cats)
 %     IN:
 %       - `labs` (fcat)
 %       - `cats` (cell array of strings, char)
+%       - `always` (cell array of strings, char) |OPTIONAL|
 %     OUT:
 %       - `c` (cell array of strings)
 
 c = cssetdiff( cats, getcats(labs, 'un') );
-if ( isempty(c) ), c = cats; end
+
+if ( isempty(c) )
+  c = cats; 
+end
+
+if ( nargin > 2 )
+  c = csunion( c, always );
+end
 
 end
