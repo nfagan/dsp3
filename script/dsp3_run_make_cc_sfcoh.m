9791f66aef46a339002c56ddea966200afa5c2e0
nfagan <nick@fagan.org>
2019-02-20 16:17:33 -0500

    add runner

---
+++ script/dsp3_run_make_cc_sfcoh.m
@@ -0,0 +1,26 @@
+conf = dsp3.config.load();
+
+dr = dsp3.dataroot( conf );
+
+cc_spike_data = load( fullfile(dr, 'public', 'cc_sfcoh', 'dictator_game_SUAdata_pre.mat') );
+
+%%
+
+linear_spike_data = dsp3_linearize_cc_sua_data( cc_spike_data );
+
+consolidated = dsp3.get_consolidated_data( conf );
+events = consolidated.events;
+event_key = consolidated.event_key;
+
+event_times = events.data(:, event_key('targAcq'));
+event_labels = fcat.from( events.labels );
+
+%%
+
+spike_times = linear_spike_data.spike_times;
+spike_labels = linear_spike_data.spike_labels;
+
+dsp3_make_cc_sf_coh( spike_times, spike_labels, event_times, event_labels ...
+  , 'is_parallel', true ...
+  , 'config', conf ...
+);
