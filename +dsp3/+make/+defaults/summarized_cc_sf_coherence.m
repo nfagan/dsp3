18d1f1c6065e172804bcd21490d0457591f26793
nfagan <nick@fagan.org>
2019-02-22 16:08:11 -0500

    add missing params

--- +dsp3/+make/+defaults/summarized_cc_sf_coherence.m
+++ +dsp3/+make/+defaults/summarized_cc_sf_coherence.m
@@ -2,6 +2,8 @@ function defaults = summarized_cc_sf_coherence()
 
 defaults = dsp3.get_common_make_defaults();
 defaults.config = dsp3.config.load();
+defaults.overwrite = false;
+defaults.is_parallel = true;
 defaults.epoch = 'targacq';
 
 end
