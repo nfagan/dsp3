f9da177dde5b894d0d19b1e367632aba62d4bd48
nfagan <nick@fagan.org>
2019-08-13 17:18:14 -0400

    add cluster run spike field

---
+++ script/dsp3_cluster_run_sfcoherence.m
@@ -0,0 +1,9 @@
+repadd( 'chronux', true );
+
+conf = dsp3.config.load();
+
+[spike_ts, spike_labels] = dsp3_load_cc_spike_data_for_sfcoherence( conf );
+
+dsp3.save_sfcoherence( 'targAcq-150', spike_ts, spike_labels' ...
+  , 'reference_func', @dsp3.bipoloar_derivation_reference ...
+);
