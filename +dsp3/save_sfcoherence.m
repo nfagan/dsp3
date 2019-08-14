1fb2073e33ee64054c0bd835ca34f66c60f7899f
nfagan <nick@fagan.org>
2019-08-14 11:33:35 -0400

    add output subdir spec

--- +dsp3/save_sfcoherence.m
+++ +dsp3/save_sfcoherence.m
@@ -1,13 +1,13 @@
-function results = save_sfcoherence(subdir, spike_ts, spike_labels, varargin)
+function results = save_sfcoherence(input_subdir, output_subdir, spike_ts, spike_labels, varargin)
 
 defaults = dsp3.make.defaults.coherence();
 
-inputs = { fullfile('aligned_lfp', subdir) };
-output = fullfile( 'per_trial_spike_field_coherence', subdir );
+inputs = { fullfile('aligned_lfp', input_subdir) };
+output = fullfile( 'per_trial_spike_field_coherence', output_subdir );
 
 [params, runner] = dsp3.get_params_and_loop_runner( inputs, output, defaults, varargin );
 runner.save_func = @(path, var) save(path, 'var', '-v7.3');
 
-results = runner.run( @dsp3.make.sfcoherence, subdir, spike_ts, spike_labels, params );
+results = runner.run( @dsp3.make.sfcoherence, input_subdir, spike_ts, spike_labels, params );
 
 end
