08e245231aeca8294c0d0feb3c4aafb97d20da76
nfagan <nick@fagan.org>
2019-08-14 16:32:46 -0400

    change defaults

--- script/dsp3_save_summarized_psd.m
+++ script/dsp3_save_summarized_psd.m
@@ -2,7 +2,7 @@ function dsp3_save_summarized_psd(varargin)
 
 summary_spec = { 'days', 'regions', 'channels', 'administration', 'outcomes', 'trialtypes', 'unit_uuid' };
 
-psd_defaults = dsp3.make.defaults.psd();
+psd_defaults = dsp3.make.defaults.summarized_psd();
 psd_defaults.summary_spec = summary_spec;
 
 params = dsp3.parsestruct( psd_defaults, varargin );
