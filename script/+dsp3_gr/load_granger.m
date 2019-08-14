204d5cc120e759b57f5fb3434c88ce4477e3d987
nfagan <nick@fagan.org>
2019-08-14 12:31:14 -0400

    summarized original ref method

--- script/+dsp3_gr/load_granger.m
+++ script/+dsp3_gr/load_granger.m
@@ -4,7 +4,35 @@ if ( nargin < 2 )
   conf = dsp3.config.load();
 end
 
-filepath = fullfile( dsp3.dataroot(conf), 'analyses', 'granger', subdir, 'granger.mat' );
-granger = shared_utils.io.fload( filepath );
+granger_p = fullfile( dsp3.dataroot(conf), 'analyses', 'granger', subdir );
+
+granger_full_filepath = fullfile( granger_p, 'granger.mat' );
+
+if ( shared_utils.io.fexists(granger_full_filepath) )
+  granger = shared_utils.io.fload( granger_full_filepath );
+else
+  granger = load_multipart_granger( granger_p );
+end
+
+end
+
+function granger = load_multipart_granger(granger_p)
+
+mats = shared_utils.io.findmat( granger_p );
+granger = [];
+
+for i = 1:numel(mats)
+  if ( i == 1 )
+    granger = shared_utils.io.fload( mats{i} );
+  else
+    granger(i) = shared_utils.io.fload( mats{i} );
+    granger(i).f = granger(i).f(1:501);
+    granger(i).t = granger(i).t(1, :);
+  end
+end
+
+if ( ~isempty(granger) )
+  granger = shared_utils.struct.soa( granger );
+end
 
 end
